fea3b3cc7afdcbc0f14a1641da07a8675fa573bf
Aaron Chesley <achesley@crossroadstech.net>
2017-03-03 08:57:48 -0700

    setup

--- src/app/books_and_run/books_and_run_play.component.ts
+++ src/app/books_and_run/books_and_run_play.component.ts
@@ -14,101 +14,17 @@ import { BooksAndRunService } from '../books_and_run.service';
 export class BooksAndRunPlayComponent implements OnInit {
   constructor(private booksAndRunService: BooksAndRunService) { }
 
-  players = [];
-  rounds = [];
-  roundOneWinner = {};
-  roundTwoWinner = {};
-  roundThreeWinner = {};
-  roundFourWinner = {};
-  roundFiveWinner = {};
-  roundSixWinner = {};
-  roundSevenWinner = {};
-  gameWinner = {};
+  game = {};
 
 
 
   ngOnInit(): void {
-    this.players = this.booksAndRunService.getPlayers();
-    this.players = this.booksAndRunService.preparePlayers(this.players);
-    this.rounds = this.booksAndRunService.prepareRounds();
+    // this.players = this.booksAndRunService.getPlayers();
+    // this.players = this.booksAndRunService.preparePlayers(this.players);
+    // this.rounds = this.booksAndRunService.prepareRounds();
   }
 
-  calculateStats(): void {
-    var roundOneWinner = null;
-    var roundTwoWinner = null;
-    var roundThreeWinner = null;
-    var roundFourWinner = null;
-    var roundFiveWinner = null;
-    var roundSixWinner = null;
-    var roundSevenWinner = null;
-    var gameWinner = null;
-
-    var winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].round1 < winner.round1){
-        winner = this.players[i]
-      }
-    }
-    this.roundOneWinner = winner;
-
-    winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].round2 < winner.round2){
-        winner = this.players[i]
-      }
-    }
-    this.roundTwoWinner = winner;
-
-    winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].round3 < winner.round3){
-        winner = this.players[i]
-      }
-    }
-    this.roundThreeWinner = winner;
-
-    winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].round4 < winner.round4){
-        winner = this.players[i]
-      }
-    }
-    this.roundFourWinner = winner;
-
-    winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].round5 < winner.round5){
-        winner = this.players[i]
-      }
-    }
-    this.roundFiveWinner = winner;
-
-    winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].round6 < winner.round6){
-        winner = this.players[i]
-      }
-    }
-    this.roundSixWinner = winner;
-
-    winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].round7 < winner.round7){
-        winner = this.players[i]
-      }
-    }
-    this.roundSevenWinner = winner;
-
-    winner = this.players[0];
-    for(var i=0; i<this.players.length; i++){
-      if(this.players[i].total < winner.total){
-        winner = this.players[i]
-      }
-    }
-    this.gameWinner = winner;
-
-    
-  }
+  
 
 
 
